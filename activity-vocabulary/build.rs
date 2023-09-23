87c46164d7fdced9ac1f94c1a9b34ffcbb7ccf7c
Masaki Nakano <admin@namachan10777.dev>
2023-09-24 06:02:17 +0900

    build cache

--- activity-vocabulary/build.rs
+++ activity-vocabulary/build.rs
@@ -4,5 +4,6 @@ fn main() {
     let src = activity_vocabulary_derive::define_types("vocab.yml").unwrap();
     let out_path = env::var("OUT_DIR").unwrap();
     let out_path: &Path = out_path.as_ref();
+    println!("cargo:rerun-if-changed=vocab.yml");
     fs::write(out_path.join("vocab.rs"), src.as_bytes()).unwrap();
 }
